d50e41829f26d2eb99e66de51fdcd65cb816e180
Luc4st1574 <n00281417@upn.pe>
2024-06-03 10:23:13 -0500

    add:screens design

--- ecoupn/lib/main.dart
+++ ecoupn/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:ecoupn/pages/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,11 +11,9 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
     );
+
   }
 }
